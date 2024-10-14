f7659a77eb693329797418edaf8dc498100a460d
Vai <vai.sriv@icloud.com>
2024-10-14 15:08:05 -0400

    added more rss feeds

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@ mod rss_read;
 
 #[tokio::main]
 async fn main() {
-    let urls = vec!["https://rss.nytimes.com/services/xml/rss/nyt/ProFootball.xml"];
+    let urls = vec![
+        "https://rss.nytimes.com/services/xml/rss/nyt/ProFootball.xml",
+        "https://api.foxsports.com/v2/content/optimized-rss?partnerKey=MB0Wehpmuj2lUhuRhQaafhBjAJqaPU244mlTDK1i&size=30&tags=fs/nfl",
+        "https://www.espn.com/espn/rss/nfl/news",
+        "https://www.cbssports.com/rss/headlines/nfl/"
+    ];
 
     match rss_read::update_feeds(urls).await {
         Ok(df) => println!("{:?}", df),
